c8a23bd787d7a38aa7a008b56a9f8f35a5d15be0
Matthias Staudinger <matthias.staudinger@uberall.com>
2022-06-08 13:42:42 +0200

    clean up code

--- README.md
+++ README.md
@@ -4,16 +4,10 @@ The uberall store-locator works out of the box for server-side-rendered pages, b
 
 This repo is an example app to showcase how to integrate our store-locator inside a React app.
 
-## Available Scripts
+## How to start the app
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+- add your storefinder-key to the [store-locator component](https://github.com/uberall/storelocator-spa-integration/blob/main/src/routes/RouteWithStoreLocator.js)
 
-In the project directory, you can run:
+- In the project directory, you can run: `npm start`
 
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+- Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
--- src/routes/RouteWithStoreLocator.js
+++ src/routes/RouteWithStoreLocator.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useLayoutEffect } from "react";
 
-import Storefinder from "./Storefinder";
 const RouteWithStoreLocator = ({
   isStoreFinderBundleLoaded,
   setIsStoreFinderBundleLoaded,
@@ -30,19 +29,15 @@ const RouteWithStoreLocator = ({
   return (
     <div
       id="store-finder-widget"
-      data-key="4w3OLJTTT66unD30WlbJhuit7Hd45w"
+      data-key="YOUR_STOREFINDER_KEY"
       data-language="en"
       data-baseurl="https://uberall.com/"
-      data-bundleurl="http://localhost:3001/store-finder-widget-bundle-v2-modern.js"
       data-showheader="true"
       data-showbrands="true"
       data-showservices="true"
       data-footertext="Uberall Storefinder Test Footer"
       data-title="Uberall Storefinder"
-      data-locationpinurl="https://static-prod.uberall.com/assets/symbol.svg"
-      data-showzipcity="true"
-      data-filters="services,locatorZip"
-      data-filter-services=""
+      data-filters="services"
     ></div>
   );
 };
